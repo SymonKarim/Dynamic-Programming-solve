f7ae91e1579f281c5685e08b167fba69280ae06c
Symon karim <symon.karim01@gmail.com>
2023-02-28 00:26:08 +0600

    Update 1143. Longest Common Subsequence.cpp

--- 1143. Longest Common Subsequence.cpp
+++ 1143. Longest Common Subsequence.cpp
@@ -1,4 +1,3 @@
-//
 class Solution {
 public:
 int sz = 0,tz =0;
@@ -27,10 +26,24 @@ int sz = 0,tz =0;
             }
         }return dp[0][0];
     }
+     int solveTabularSpaceOptimized(string &s, string &t){
+       vector<int>next(s.size()+1,0),curr(s.size()+1,0);
+        for(int i=sz-1;i>=0;i--){
+            for(int j=tz-1;j>=0;j--){
+                 int x = 0;
+                 if(s[i]==t[j])
+                      x = 1+ next[j+1];
+                 else x = max(next[j], curr[j+1]);
+                 curr[j] = x;
+            }
+            next = curr;
+        }return next[0];
+    }
     int longestCommonSubsequence(string text1, string text2) {
          sz = text1.length(), tz = text2.length();
         // vector<vector<int>>dp(sz,vector<int>(tz,-1));
         // return solve(text1,text2,0,0,dp);
         return solveTabular(text1,text2);
+        return solveTabularSpaceOptimized(text1,text2);
     }
 };
